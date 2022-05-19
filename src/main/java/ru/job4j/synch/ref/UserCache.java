2f5d7d27eef8a82ab469ea424901374326745cda
denvoiten <den.voiten@gmail.com>
2022-05-19 15:12:24 +0300

    4. Thread без общих ресурсов [#267919]

---
+++ src/main/java/ru/job4j/synch/ref/UserCache.java
@@ -0,0 +1,28 @@
+package ru.job4j.synch.ref;
+
+import net.jcip.annotations.NotThreadSafe;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@NotThreadSafe
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        List<User> list = new ArrayList<>();
+        users.values().forEach(value -> list.add(User.of(value.getName())));
+        return list;
+    }
+}
