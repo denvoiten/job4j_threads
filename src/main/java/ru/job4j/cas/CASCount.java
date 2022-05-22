a4a2a807015c360f053c399fa10675c3c1064d16
denvoiten <den.voiten@gmail.com>
2022-05-22 16:54:42 +0300

    0. CAS - операции [#6859]

---
+++ src/main/java/ru/job4j/cas/CASCount.java
@@ -0,0 +1,21 @@
+package ru.job4j.cas;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+@ThreadSafe
+public class CASCount {
+    private final AtomicReference<Integer> count = new AtomicReference<>();
+
+    public void increment() {
+        int oldValue;
+        do {
+            oldValue = count.get();
+        } while (!count.compareAndSet(oldValue, oldValue + 1));
+    }
+
+    public int get() {
+        return count.get();
+    }
+}
