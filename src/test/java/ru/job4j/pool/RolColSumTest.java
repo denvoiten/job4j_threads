71ff13e710b9427d42bb9ec6f4a5a2281adece4d
denvoiten <den.voiten@gmail.com>
2022-05-24 17:25:44 +0300

    4. CompletableFuture [#361626 #304655]

--- src/test/java/ru/job4j/pool/RolColSumTest.java
+++ src/test/java/ru/job4j/pool/RolColSumTest.java
@@ -6,6 +6,7 @@ import java.util.concurrent.ExecutionException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static ru.job4j.pool.RolColSum.asyncSum;
 import static ru.job4j.pool.RolColSum.sum;
 
 public class RolColSumTest {
@@ -16,12 +17,12 @@ public class RolColSumTest {
 
     @Test
     public void whenAsyncSum() throws ExecutionException, InterruptedException {
-        assertThat(RolColSum.asyncSum(matrix)[0].getRowSum(), equalTo(44));
-        assertThat(RolColSum.asyncSum(matrix)[0].getColSum(), equalTo(9));
-        assertThat(RolColSum.asyncSum(matrix)[1].getRowSum(), equalTo(25));
-        assertThat(RolColSum.asyncSum(matrix)[1].getColSum(), equalTo(46));
-        assertThat(RolColSum.asyncSum(matrix)[2].getRowSum(), equalTo(26));
-        assertThat(RolColSum.asyncSum(matrix)[2].getColSum(), equalTo(40));
+        assertThat(asyncSum(matrix)[0].getRowSum(), equalTo(44));
+        assertThat(asyncSum(matrix)[0].getColSum(), equalTo(9));
+        assertThat(asyncSum(matrix)[1].getRowSum(), equalTo(25));
+        assertThat(asyncSum(matrix)[1].getColSum(), equalTo(46));
+        assertThat(asyncSum(matrix)[2].getRowSum(), equalTo(26));
+        assertThat(asyncSum(matrix)[2].getColSum(), equalTo(40));
     }
 
     @Test
